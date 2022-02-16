dcc420f64906d5f5767e891740333f5a1a904cc2
akifzsche <akif.tonmaister58@gmail.com>
2022-02-16 15:23:40 +0300

    item silme tamamlandı. Kodlar düzenlenecek. Proje şimdilik bitti

--- public/js/script.js
+++ public/js/script.js
@@ -1,87 +1,174 @@
 var data = [];
-let plan = {
 
-    ekle: function () {
+function dailyData() {
 
+    var Dailyinside = {
+        Dailyinside: document.querySelector("#inputdaily").value,
 
-    },
+    };
 
+    if (Dailyinside.inputdaily !== "") {
+        data = JSON.parse(localStorage.getItem("dailydata")) || [];
+        data.push(Dailyinside);
 
-
-    dailyData: function () {
-
-        var Dailyinside = {
-            Dailyinside: document.querySelector("#inputdaily").value,
-        };
-        if (Dailyinside.inputdaily !== "") {
-            data = JSON.parse(localStorage.getItem("dailydata")) || [];
-            data.push(Dailyinside);
-            localStorage.setItem("dailydata", JSON.stringify(data));
-            // data = JSON.parse(data);
-
-            function getdata() {
-                return JSON.parse(localStorage.getItem("dailydata"));
-            }
-            function init() {
-                var uye = getdata();
-
-                for (i = 0; i < data.length; i++) {
-                    var daily = data[i];
-                    createSchedule(daily);
-                }
-            }
-
-        }
-
-        document.querySelector(".dailycontent").innerHTML += '<br><a class="deletedata" href="">' + document.getElementById("inputdaily").value + '</a>';
-
-
-    },
-
-
-
-    weeklyData: function () {
-        var W = document.querySelector("#weekly").value;
-        localStorage.setItem('weekly', W);
-        document.querySelector(".weeklycontent").innerText = localStorage.getItem('weekly');
-    },
-    monthlyData: function () {
-        var M = document.querySelector("#monthly").value;
-        localStorage.setItem('monthly', M);
-        document.querySelector(".monthlycontent").innerText = localStorage.getItem('monthly');
+        localStorage.setItem("dailydata", JSON.stringify(data));
+        // data = JSON.parse(data);
     }
 
 
+
+
+    //inputtan aldığı veriyi card içine bastım
+    inputdata = document.getElementById("inputdaily").value;
+
+    document.querySelector(".dailycontent").innerHTML += '<br>' + `<span class="deletedata">` + inputdata;
+
+    document.getElementById('inputdaily').value = "";
+
 }
-//eğer localde bir data varsa yüklemeye başla
+
+
+
+function weeklyData() {
+    var Weeklyinside = {
+        Weeklyinside: document.querySelector("#inputweekly").value,
+
+    };
+
+    if (Weeklyinside.inputweekly !== "") {
+        data = JSON.parse(localStorage.getItem("weeklydata")) || [];
+        data.push(Weeklyinside);
+
+        localStorage.setItem("weeklydata", JSON.stringify(data));
+        // data = JSON.parse(data);
+    }
+
+
+
+
+    //inputtan aldığı veriyi card içine bastım
+    inputdata = document.getElementById("inputweekly").value;
+
+    document.querySelector(".weeklycontent").innerHTML += '<br>' + `<span class="deletedata">` + inputdata;
+
+    document.getElementById('inputweekly').value = "";
+
+
+
+
+
+}
+function monthlyData() {
+    var monthlyinside = {
+        monthlyinside: document.querySelector("#inputmonthly").value,
+
+    };
+
+    if (monthlyinside.inputmonthly !== "") {
+        data = JSON.parse(localStorage.getItem("monthlydata")) || [];
+        data.push(monthlyinside);
+
+        localStorage.setItem("monthlydata", JSON.stringify(data));
+        // data = JSON.parse(data);
+    }
+
+
+
+
+    //inputtan aldığı veriyi card içine bastım
+    inputdata = document.getElementById("inputmonthly").value;
+
+    document.querySelector(".monthlycontent").innerHTML += '<br>' + `<span class="deletedata">` + inputdata;
+
+    document.getElementById('inputmonthly').value = "";
+
+
+
+
+}
+
+
+
+// eğer localde bir data varsa yüklemeye başla
 if (localStorage.getItem('dailydata') != null) {
     // sayfa açılınca yüklenme metodu
-    var temp = JSON.parse(localStorage.getItem('dailydata'));
-    temp.map((item) => {
-        document.querySelector(".dailycontent").innerHTML += '<br>' + '<a class="deletedata" href="">' + item.Dailyinside + '</a>';
+    var dailytemp = JSON.parse(localStorage.getItem('dailydata'));
+    dailytemp.map((item, index) => {
+        document.querySelector(".dailycontent").innerHTML += '<br>' + `<span class="deletedata" id=${index} onclick=deleteplan(id)>` + item.Dailyinside;
 
     })
 }
 
+if (localStorage.getItem('weeklydata') != null) {
+    // sayfa açılınca yüklenme metodu
+    var weeklytemp = JSON.parse(localStorage.getItem('weeklydata'));
+    weeklytemp.map((item, index) => {
+        document.querySelector(".weeklycontent").innerHTML += '<br>' + `<span class="deletedata" id=${index} onclick=deleteWeeklyPlan(id)>` + item.Weeklyinside;
 
+    })
+}
+if (localStorage.getItem('monthlydata') != null) {
+    // sayfa açılınca yüklenme metodu
+    var monthlytemp = JSON.parse(localStorage.getItem('monthlydata'));
+    monthlytemp.map((item, index) => {
+        document.querySelector(".monthlycontent").innerHTML += '<br>' + `<span class="deletedata" id=${index} onclick=deleteMonthlyPlan(id)>` + item.monthlyinside;
 
+    })
+}
 
+function deleteplan(data) {
 
+    dailytemp.map((item, index) => {
+        if (index == data)
+            dailytemp.splice(index, 1);
+
+    })
+    localStorage.setItem("dailydata", JSON.stringify(dailytemp))
+
+    location.reload();
+
+}
+
+function deleteWeeklyPlan(data) {
+
+    weeklytemp.map((item, index) => {
+        if (index == data)
+            weeklytemp.splice(index, 1);
+
+    })
+    localStorage.setItem("weeklydata", JSON.stringify(weeklytemp))
+
+    location.reload();
+}
+
+function deleteMonthlyPlan(data) {
+    monthlytemp.map((item, index) => {
+        if (index == data)
+            monthlytemp.splice(index, 1);
+
+    })
+    localStorage.setItem("monthlydata", JSON.stringify(monthlytemp))
+
+    location.reload();
+}
 
 document.querySelector("#inputdaily").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
 
-        plan.dailyData();
+        dailyData();
+        location.reload();
     }
 });
-document.querySelector("#weekly").addEventListener("keyup", function (event) {
+document.querySelector("#inputweekly").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
-        plan.weeklyData();
+        weeklyData();
+        location.reload();
     }
 });
-document.querySelector("#monthly").addEventListener("keyup", function (event) {
+document.querySelector("#inputmonthly").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
-        plan.monthlyData();
+        monthlyData();
+        location.reload();
     }
 });
 
